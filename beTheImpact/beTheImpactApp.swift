36e613ac3283f8c624732b78dc40d916f61faec8
raneem <ixranem7@gmail.com>
2024-12-18 11:06:18 +0300

    Initial Commit

---
+++ beTheImpact/beTheImpactApp.swift
@@ -0,0 +1,17 @@
+//
+//  beTheImpactApp.swift
+//  beTheImpact
+//
+//  Created by Raneem on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct beTheImpactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ beTheImpactTests/beTheImpactTests.swift
@@ -0,0 +1,17 @@
+//
+//  beTheImpactTests.swift
+//  beTheImpactTests
+//
+//  Created by Raneem on 17/06/1446 AH.
+//
+
+import Testing
+@testable import beTheImpact
+
+struct beTheImpactTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

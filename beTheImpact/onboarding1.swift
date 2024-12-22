70a2ba916ca1c33feb0cd9bec52db20048488180
raneem <ixranem7@gmail.com>
2024-12-22 12:51:54 +0300

    splash

---
+++ beTheImpact/onboarding1.swift
@@ -0,0 +1,18 @@
+//
+//  onboarding1.swift
+//  beTheImpact
+//
+//  Created by Raneem on 17/06/1446 AH.
+//
+
+import SwiftUI
+
+struct onboarding1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    onboarding1()
+}
